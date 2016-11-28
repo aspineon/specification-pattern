c3d562bdc8e891ff3535e0857af1d4109b0c732f
David Landa <david.landa@rozhlas.cz>
2016-11-28 17:37:44 +0100

    Add some doc comments

--- specification/src/main/java/io/github/uetoyo/patterns/specification/CompositeSpecification.java
+++ specification/src/main/java/io/github/uetoyo/patterns/specification/CompositeSpecification.java
@@ -12,12 +12,25 @@ public abstract class CompositeSpecification<T> extends AbstractSpecification<T>
 	private final Specification<T> first;
 	private final Specification<T> second;
 	
+	/**
+	 * Creates a new composite specification with a given id.
+	 *
+	 * @param uniqueId
+	 * @param first The first specification (left-hand side).
+	 * @param second The second specification (right-hand side).
+	 */
 	protected CompositeSpecification(UUID uniqueId, Specification<T> first, Specification<T> second) {
 		super(uniqueId);
 		this.first = first;
 		this.second = second;
 	}
 	
+	/**
+	 * Creates a new composite specification with an auto generated id.
+	 * 
+	 * @param first The first specification (left-hand side).
+	 * @param second The second specification (right-hand side).
+	 */
 	protected CompositeSpecification(Specification<T> first, Specification<T> second) {
 		this(UUID.randomUUID(), first, second);
 	}
