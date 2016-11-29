d26a82ea5dee7663de9be55f3a271177ded97bd3
David Landa <david.landa@protonmail.com>
2016-11-29 13:13:56 +0100

    Add null value check

--- src/main/java/io/github/uetoyo/patterns/specification/LeafSpecification.java
+++ src/main/java/io/github/uetoyo/patterns/specification/LeafSpecification.java
@@ -5,6 +5,9 @@ public abstract class LeafSpecification<T> implements Specification<T> {
 	protected final Specification<T> spec;
 	
 	protected LeafSpecification(Specification<T> spec) {
+		if (spec == null) {
+			throw new IllegalArgumentException("The `null` value is not allowed!");
+		}
 		this.spec = spec;
 	}
 	
