4239b494ddb02bfa508b52e2b6b591a839a30134
David Landa <david.landa@rozhlas.cz>
2017-02-28 15:05:20 +0100

    Minor changes

--- src/main/java/io/github/uetoyo/patterns/specification/ConjunctionSpecification.java
+++ src/main/java/io/github/uetoyo/patterns/specification/ConjunctionSpecification.java
@@ -1,9 +1,11 @@
 package io.github.uetoyo.patterns.specification;
 
-/* 
+/** 
  * Conjunction specification represents the logical AND.
+ * 
+ * @param <T> The type of entity for which the specification is defined.
  */
-public final class ConjunctionSpecification<T> extends BinaryCompositeSpecification<T> {
+final class ConjunctionSpecification<T> extends BinaryCompositeSpecification<T> {
 	
 	public ConjunctionSpecification(final Specification<T> spec1, final Specification<T> spec2) {
 		super(spec1, spec2);
@@ -12,6 +14,7 @@ public final class ConjunctionSpecification<T> extends BinaryCompositeSpecificat
 	/**
 	 * {@inheritDoc}
 	 */
+	@Override
 	public boolean isSatisfiedBy(final T candidate) {
 		return getSpecificationOne().isSatisfiedBy(candidate) && getSpecificationTwo().isSatisfiedBy(candidate);
 	}
