79ad2a7fd04dca356d054976020001723bdee3f8
David Landa <david.landa@protonmail.com>
2017-03-06 16:15:44 +0100

    Merge the tests

--- src/test/java/io/github/uetoyo/patterns/specification/SpecificationTest.java
+++ src/test/java/io/github/uetoyo/patterns/specification/SpecificationTest.java
@@ -1,18 +1,37 @@
 package io.github.uetoyo.patterns.specification;
 
 import static org.hamcrest.CoreMatchers.instanceOf;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
+
+import java.util.Set;
 
 import org.junit.Test;
 
-import io.github.uetoyo.patterns.specification.AlwaysFalseSpecification;
-import io.github.uetoyo.patterns.specification.AlwaysTrueSpecification;
-import io.github.uetoyo.patterns.specification.ConjunctionSpecification;
-import io.github.uetoyo.patterns.specification.DisjunctionSpecification;
-import io.github.uetoyo.patterns.specification.NegationSpecification;
-import io.github.uetoyo.patterns.specification.Specification;
+public class SpecificationTest {
+	
+	private final AlwaysTrueSpecification<Object> ts = 
+			Specification.<Object>createAlwaysTrueSpecification();
+	
+	private final AlwaysFalseSpecification<Object> fs = 
+			Specification.<Object>createAlwaysFalseSpecification();
+	
+	@Test public void alwaysTrueSpecificationsShouldBeEqual() {
+		assertTrue(ts.equals(ts));
+	}
+	
+	@Test public void alwaysFalseSpecificationsShouldBeEqual() {
+		assertTrue(fs.equals(fs));
+	}
 
-public class SpecificationTest extends SpecificationTestBase {
+	@Test public void alwaysTrueAndFalseSpecificationsShouldBeNotEqual() {
+		assertFalse(ts.equals(fs));
+		assertFalse(fs.equals(ts));
+		assertFalse(fs.hashCode() == ts.hashCode());
+	}
+	
+	// Static factory methods
 	
 	@Test
 	public void shouldCreateAlwaysFalseSpecificaton() {
@@ -42,5 +61,114 @@ public class SpecificationTest extends SpecificationTestBase {
 	public void shouldCreateNegationSpecificaton() {
 		assertThat(Specification.<Object>createNegationSpecification(ts), 
 				instanceOf(NegationSpecification.class));
+	}
+	
+	// Negation
+	
+	@Test
+	public void testThatNegationReturnsFalse() {
+		NegationSpecification<Object> spec = new NegationSpecification<>(ts);
+		assertFalse(spec.isSatisfiedBy(new Object()));
+	}
+	
+	@Test
+	public void testThatNegationReturnsTrue() {
+		NegationSpecification<Object> spec = new NegationSpecification<>(fs);
+		assertTrue(spec.isSatisfiedBy(new Object()));
+	}
+	
+	/* 
+	 * True AND True = True
+	 */
+	@Test
+	public void testThatConjunctionReturnsTrueForTrueAndTrueSpecification() {
+		ConjunctionSpecification<Object> spec = new ConjunctionSpecification<>(ts, ts);
+		assertTrue(spec.isSatisfiedBy(new Object()));
+	}
+	
+	/*
+	 * True AND False = False
+	 */
+	@Test
+	public void testThatConjunctionReturnsFalseForTrueAndFalseSpecification() {
+		ConjunctionSpecification<Object> spec = new ConjunctionSpecification<>(ts, fs);
+		assertFalse(spec.isSatisfiedBy(new Object()));
+	}
+	
+	/*
+	 * False AND True = False
+	 */
+	@Test
+	public void testThatConjunctionReturnsFalseForFalseAndTrueSpecification() {
+		ConjunctionSpecification<Object> spec = new ConjunctionSpecification<>(fs, ts);
+		assertFalse(spec.isSatisfiedBy(new Object()));
+	}
+	
+	/*
+	 * False AND False = False
+	 */
+	@Test
+	public void testThatConjunctionReturnsFalseForFalseAndFalseSpecification() {
+		ConjunctionSpecification<Object> spec = new ConjunctionSpecification<>(fs, fs);
+		assertFalse(spec.isSatisfiedBy(new Object()));
+	}
+	
+	/*
+	 * To predicate conversion.
+	 */
+	@Test 
+	public void shouldReturnSpecificationAsPredicate() {
+		ConjunctionSpecification<Object> spec = new ConjunctionSpecification<>(ts, ts);
+		assertTrue(spec.toPredicate().test(new Object()));
+	}
+	
+	/* 
+	 * True OR True = True
+	 */
+	@Test
+	public void testThatDisjunctionReturnsTrueForTrueOrTrueSpecification() {
+		DisjunctionSpecification<Object> spec = new DisjunctionSpecification<>(ts, ts);
+		assertTrue(spec.isSatisfiedBy(new Object()));
+	}
+	
+	/*
+	 * True OR False = True
+	 */
+	@Test
+	public void testThatDisjunctionReturnsTrueForTrueOrFalseSpecification() {
+		DisjunctionSpecification<Object> spec = new DisjunctionSpecification<>(ts, fs);
+		assertTrue(spec.isSatisfiedBy(new Object()));
+	}
+	
+	/*
+	 * False OR True = True
+	 */
+	@Test
+	public void testThatDisjunctionReturnsTrueForFalseOrTrueSpecification() {
+		DisjunctionSpecification<Object> spec = new DisjunctionSpecification<>(fs, ts);
+		assertTrue(spec.isSatisfiedBy(new Object()));
+	}
+	
+	/*
+	 * False AND False = False
+	 */
+	@Test
+	public void testThatDisjunctionReturnsFalseForFalseOrFalseSpecification() {
+		DisjunctionSpecification<Object> spec = new DisjunctionSpecification<>(fs, fs);
+		assertFalse(spec.isSatisfiedBy(new Object()));
+	}
+	
+	@Test
+	public void shouldReturnUnsatisfiedSpecification() {
+		CompositeSpecification<Object> spec = new ConjunctionSpecification<Object>(ts, fs);
+		Set<Specification<Object>> unsatisfiedSpecs  = spec.getUnsatisfiedSpecificationsFor(new Object());
+		assertTrue(unsatisfiedSpecs.size() == 1);
+	}
+	
+	@Test
+	public void shouldReturnOnlyOneUnsatisfiedSpecificationIfTheAreOfSameType() {
+		CompositeSpecification<Object> spec = new ConjunctionSpecification<Object>(fs, fs);
+		Set<Specification<Object>> unsatisfiedSpecs  = spec.getUnsatisfiedSpecificationsFor(new Object());
+		assertTrue(unsatisfiedSpecs.size() == 1);
 	}
 }
